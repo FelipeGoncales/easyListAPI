ee53ebd1b26e5d454ff56ffd4b2935059e52b113
Felipe Gonçales <felipeggoncales22@gmail.com>
2025-08-19 13:53:47 -0300

    validar token

--- login_view.py
+++ login_view.py
@@ -1,10 +1,6 @@
-from http.client import responses
-
 from flask import Flask, request, jsonify
 import sqlite3
-
 from flask_bcrypt import check_password_hash
-
 from main import app, connectDb, SENHA_SECRETA
 import jwt
 
@@ -17,6 +13,48 @@ def generateToken(idUsuario, email):
     token = jwt.encode(payload, SENHA_SECRETA, algorithm='HS256')
 
     return token
+
+def remover_bearer(token):
+    if token.startswith('Bearer '):
+        return token[len('Bearer '):]
+    else:
+        return token
+
+@app.route('/login', methods=['GET'])
+def get_login():
+    token = request.headers.get('Authorization')
+
+    if not token:
+        return jsonify({'error': 'Token de autenticação necessário'}), 401
+
+    token = remover_bearer(token)
+
+    try:
+        payload = jwt.decode(token, SENHA_SECRETA, algorithms=['HS256'])
+        id_usuario = payload['id_usuario']
+    except jwt.ExpiredSignatureError:
+        return jsonify({'error': 'Token expirado'}), 401
+    except jwt.InvalidTokenError:
+        return jsonify({'error': 'Token inválido'}), 401
+
+    con = connectDb()
+
+    cursor = con.cursor()
+
+    cursor.execute('''
+        SELECT 1 
+        FROM USUARIOS
+        WHERE ID_USUARIO = ?    
+    ''', (id_usuario,))
+
+    if cursor.fetchone():
+        return jsonify({
+            'userExist': True
+        }), 200
+
+    return jsonify({
+        'userExist': False
+    }), 200
 
 @app.route('/login', methods=['POST'])
 def login():
